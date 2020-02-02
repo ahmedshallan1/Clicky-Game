cd2a1f7f6424de6fc26b00c7122b676085dc2d53
Ahmed Shallan <ahmedshallan@Ahmeds-MacBook-Air.local>
2020-02-01 19:00:26 -0500

    Updates

---
+++ precache-manifest.ce4002b5c44c1e3d543d5f22f57a8da7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be57db28c80f09081db8425dace98f36",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "77fd0db9f412892c104a",
+    "url": "/Clicky-Game/static/css/main.1fe9626d.chunk.css"
+  },
+  {
+    "revision": "75825d200ee452fee1fd",
+    "url": "/Clicky-Game/static/js/2.37cd1f6d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky-Game/static/js/2.37cd1f6d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77fd0db9f412892c104a",
+    "url": "/Clicky-Game/static/js/main.480a02ce.chunk.js"
+  },
+  {
+    "revision": "b1f829904e6b052bbdcd",
+    "url": "/Clicky-Game/static/js/runtime-main.64946b06.js"
+  }
+]);
